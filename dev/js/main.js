b34045455e5e7ef1daa1747f08ede0c6f050b812
Sifedine <Sifedinehajji01@gmail.com>
2022-01-23 22:40:08 +0100

    finishing the burger menu css & js

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,17 +1,27 @@
+console.log("hello");
 const navSlide = () => {
-  const burger = document.querySelector(".burger-menu");
-  const nav = document.querySelector(".header");
+  const burger = document.querySelector(".menu-burger");
+  const burgerSlide = document.querySelector(".burger-slide");
 
   // ajoute une classe au click sur les element seletionner ci-dessus
   burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-    burger.classList.toggle("active");
+    burgerSlide.classList.toggle("active");
+  });
+};
+
+const closeSlide = () => {
+  const burgerSlide = document.querySelector(".burger-slide");
+  const closeBtn = document.querySelector(".close-btn");
+
+  closeBtn.addEventListener("click", () => {
+    burgerSlide.classList.toggle("active");
   });
 };
 
 // AU CAS OU ON A PLUSIEURS FONCTIONS
 const app = () => {
   navSlide();
+  closeSlide();
 };
 
 // APPEL DE LA FONCTION APP POUR EFFECTUER TOUTES NOS FONCTIONS
